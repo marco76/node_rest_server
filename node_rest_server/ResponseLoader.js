c7b6e0e98a17f7b4073a377af40213cc9e51a5c1
marco76 <moltenma@gmail.com>
2016-10-19 11:42:45 +0200

    comments

--- node_rest_server/ResponseLoader.js
+++ node_rest_server/ResponseLoader.js
@@ -1,30 +1,37 @@
 "use strict";
 
+// load the module that manages the filesystem
 var fs = require('fs');
 
 function ResponseLoader (name){
 
-    var that = this;
+    // the content of the file is stored in this variable
     var json;
 
+    // method that load the file on the base of the URL
     ResponseLoader.prototype.load = function(url){
         console.log("requested file: " + url);
+        // url: hello -> file: [current directory]/json/hello.json
         fs.readFile( __dirname + '/json' +url+'.json', function (err, data) {
             if (err) {
                 console.log('error');
                 throw err;
             }
+            // the content of the file is assigned to the variable
             json = data;
+
             console.log(name + " : " +data.toString());
         })
     };
 
+    // give me the json content
     ResponseLoader.prototype.getJson = function (){
         console.log(name + " : return json");
+
         return json;
     };
-
 }
 
+// this is node.js specific, it is required to expose the local function
 module.exports = ResponseLoader;
 
--- node_rest_server/server.js
+++ node_rest_server/server.js
@@ -6,17 +6,27 @@ var main = function main(){
     const hostname = '127.0.0.1';
     const port = 3000;
 
+    // import the function from the module, specific node.js
     var loaderModule = require('./ResponseLoader.js');
+    // create an instance of the prototype
     var loader = new loaderModule("loader");
 
     function requestService(request, response){
+        // url == filename without extension
         var url = request.url;
+
+        // home page called
         if (url=="/"){
             url = "/index";
         }
+
+        // call the method that load the static page
         loader.load(url);
+
+        // prepare the http response
         response.statusCode = 200;
         response.setHeader('Content-Type', 'application/json');
+        // add the JSON content
         response.end(loader.getJson());
     }
 
